114e42cb5342539db5e11f5e053e6d8c5b3057bd
king29 <nking5294@gmail.com>
2018-02-14 16:53:01 -0800

    reverted code to initial commit and added size command functionality

--- docs/TM.java
+++ docs/TM.java
@@ -1,12 +1,10 @@
  /* Nicholas King, CSC 131-03, Assignment 2
  * This program uses command line arguments to log start and stop times of particular task and saves the information in a text file title "tm.txt"
  * List of acceptable command line arguments can be found in TM.md provided
- */
+ */ 
 import java.io.*;
-import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.*;
 
 public class TM
@@ -38,21 +36,21 @@ public class TM
 			break;
 		default : System.out.println("Unreadable Input");
 			break;
-		}		
+		}	
 	}
 	private static void cmdStart(WriteFile file, String[] args) throws IOException {
 		TimeStamp tm = new TimeStamp();
-		Timestamp time = tm.main();
-		//saveData(time, file, args);
+		String time = tm.main();
+		saveData(time, file, args);
 	}
 	private static void cmdStop(WriteFile file, String[] args) throws IOException {
 		TimeStamp tm = new TimeStamp();
-		Timestamp time = tm.main();
-		//saveData(time, file, args);
+		String time = tm.main();
+		saveData(time, file, args);
 	}
 	private static void cmdDescribe(WriteFile file, String[] args) throws IOException {	
 		String[] description = new String[args.length-2];
-		if (args[1].equalsIgnoreCase("size")) {
+		if (args[0].equalsIgnoreCase("size")) {
 			for (int i = 2; i < args.length; i++) {
 				description[i-2] = args[i];
 			}
@@ -98,105 +96,13 @@ public class TM
 		System.out.println("Data has been saved to tm.txt");
 	}
 }
-
-class TaskDuration{
-	private TimeStamp start, stop;
-	public void TaskDuration(TimeStamp start,TimeStamp stop) {
-		this.start = start;
-		this.stop = stop;
-	}
-	
-}
-
-class Task{
-	String name;
-	String description;
-	LinkedList<TaskDuration> durations;
-	long stopTime;
-	long timeSpent;
-	public void task(String name, LinkedList<TaskLogEntry>entries) {
-		Timestamp lastStart = null;
-		this.name = name;
-		LinkedList<TaskDuration> duration = new LinkedList<TaskDuration>();
-		for (TaskLogEntry entry : entries) {
-			if (entry.name.equals(this.name)) {
-				WriteFile file = new WriteFile();
-				switch (entry.command){
-				case "start" : 
-					lastStart = TimeStamp.main();
-				case "stop" :{
-					if (lastStart != null)
-						stopTime = System.currentTimeMillis();
-						timeSpent = stopTime - lastStart.getTime();
-				}
-					
-				}
-			}
-	 	
-		}
-	}
-	public static Timestamp diff (java.util.Date t1, java.util.Date t2)
-	{
-	    // Make sure the result is always > 0
-	    if (t1.compareTo (t2) < 0)
-	    {
-	        java.util.Date tmp = t1;
-	        t1 = t2;
-	        t2 = tmp;
-	    }
-
-	    // Timestamps mix milli and nanoseconds in the API, so we have to separate the two
-	    long diffSeconds = (t1.getTime () / 1000) - (t2.getTime () / 1000);
-	    // For normals dates, we have millisecond precision
-	    int nano1 = ((int) t1.getTime () % 1000) * 1000000;
-	    // If the parameter is a Timestamp, we have additional precision in nanoseconds
-	    if (t1 instanceof Timestamp)
-	        nano1 = ((Timestamp)t1).getNanos ();
-	    int nano2 = ((int) t2.getTime () % 1000) * 1000000;
-	    if (t2 instanceof Timestamp)
-	        nano2 = ((Timestamp)t2).getNanos ();
-
-	    int diffNanos = nano1 - nano2;
-	    if (diffNanos < 0)
-	    {
-	        // Borrow one second
-	        diffSeconds --;
-	        diffNanos += 1000000000;
-	    }
-
-	    // mix nanos and millis again
-	    Timestamp result = new Timestamp ((diffSeconds * 1000) + (diffNanos / 1000000));
-	    // setNanos() with a value of in the millisecond range doesn't affect the value of the time field
-	    // while milliseconds in the time field will modify nanos! Damn, this API is a *mess*
-	    result.setNanos (diffNanos);
-	    return result;
-	}
-}
-
-class TaskLogEntry{
-	LocalDateTime start, stop;
-	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-	String name;
-	String command;
-	String data;
-	StringTokenizer stock = new StringTokenizer(command, "\n"); {
-		if (stock.countTokens() > 3) 
-			data = stock.nextToken();
-		else
-			data = "";
-	}
-	
-	
-}
-
 class TimeStamp	
 {
-	public static Timestamp main() {
+	public static String main() {
 		//current date and time retrived using DateFormat and Calendar class
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 		DateFormat df = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
 		Calendar calobj = Calendar.getInstance();
-		return (timestamp);
+		return (df.format(calobj.getTime()));
 	}
 }
 class WriteFile{
