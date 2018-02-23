48ab28136404da5bb9cb58aea9d5f0a1a7f4aa7b
king29 <nking5294@gmail.com>
2018-02-22 16:50:37 -0800

    Changed timestamp type to LocalDateTime.now() in start and stop methods

--- docs/TM.java
+++ docs/TM.java
@@ -1,10 +1,14 @@
- /* Nicholas King, CSC 131-03, Assignment 2
+  /* Nicholas King, CSC 131-03, Assignment 2
  * This program uses command line arguments to log start and stop times of particular task and saves the information in a text file title "tm.txt"
- * List of acceptable command line arguments can be found in TM.md provided, description method has been modiffied to include size command 2/12/18
- */ 
+ * List of acceptable command line arguments can be found in TM.md provided
+ */
+import java.time.*;
+import java.time.temporal.ChronoUnit;
 import java.io.*;
+import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class TM
@@ -39,14 +43,14 @@ public class TM
 		}	
 	}
 	private static void cmdStart(WriteFile file, String[] args) throws IOException {
-		TimeStamp tm = new TimeStamp();
-		String time = tm.main();
-		saveData(time, file, args);
+		String ts = LocalDateTime.now().toString();
+		LocalDateTime start = LocalDateTime.parse(ts);
+		//saveData(time, file, args);
 	}
 	private static void cmdStop(WriteFile file, String[] args) throws IOException {
-		TimeStamp tm = new TimeStamp();
-		String time = tm.main();
-		saveData(time, file, args); 
+		String ts = LocalDateTime.now().toString();
+		LocalDateTime start = LocalDateTime.parse(ts);
+		saveData(ts, file, args);
 	}
 	private static void cmdDescribe(WriteFile file, String[] args) throws IOException {	
 		String[] description = new String[args.length-2];
@@ -66,27 +70,31 @@ public class TM
 	}
 	
 	//Method retrieved 2/1/2018 from https://stackoverflow.com/questions/16169418/write-a-program-find-that-searches-all-files-specified-on-the-command-line-and-p
-	public static void cmdSummary(String[] args) throws FileNotFoundException {	
+	public static String cmdSummary(String[] args) throws FileNotFoundException {	
 		File one = new File("tm.txt");
+		String een = "";
 	    Scanner in = new Scanner(one);
 	    if (args.length > 1) {
 		    String input = args[1];
 		    while (in.hasNextLine()) {
-		        String een = in.nextLine();
+		        een = in.nextLine();
 	
 		        if (een.contains(input)) {
-		            System.out.println(een);
+		           System.out.println(een);
 		        }
 		    }
+		    return een;
 	    }
 	    else if (args.length == 1) 
 	    {
 	    	while (in.hasNextLine()) {
-		        String een = in.nextLine();
+		        een = in.nextLine();
 		        System.out.println(een);
 		    }
+	    	return een;
 	    }
 	    in.close();
+		return een;
 	}
 	
 	private static void saveData(String time, WriteFile file, String[] args) throws IOException {
@@ -96,13 +104,105 @@ public class TM
 		System.out.println("Data has been saved to tm.txt");
 	}
 }
+
+class TaskDuration{
+	private TimeStamp start, stop;
+	public void TaskDuration(TimeStamp start,TimeStamp stop) {
+		this.start = start;
+		this.stop = stop;
+	}
+	
+}
+
+class Task{
+	String name;
+	String description;
+	LinkedList<TaskDuration> durations;
+	java.util.Date startTime;
+	Timestamp stopTime;
+	Timestamp timeSpent;
+	public void task(String name, LinkedList<TaskLogEntry>entries) {
+		Timestamp lastStart = null;
+		this.name = name;
+		LinkedList<TaskDuration> duration = new LinkedList<TaskDuration>();
+		for (TaskLogEntry entry : entries) {
+			if (entry.name.equals(this.name)) {
+				WriteFile file = new WriteFile();
+				switch (entry.command){
+				case "start" : 
+					lastStart = TimeStamp.main();
+				case "stop" :{
+					if (lastStart != null)
+						stopTime = TimeStamp.main();
+				}
+					
+				}
+			}
+	 	
+		}
+	}
+	public static Timestamp diff (java.util.Date t1, java.util.Date t2)
+	{
+	    // Make sure the result is always > 0
+	    if (t1.compareTo (t2) < 0)
+	    {
+	        java.util.Date tmp = t1;
+	        t1 = t2;
+	        t2 = tmp;
+	    }
+
+	    // Timestamps mix milli and nanoseconds in the API, so we have to separate the two
+	    long diffSeconds = (t1.getTime () / 1000) - (t2.getTime () / 1000);
+	    // For normals dates, we have millisecond precision
+	    int nano1 = ((int) t1.getTime () % 1000) * 1000000;
+	    // If the parameter is a Timestamp, we have additional precision in nanoseconds
+	    if (t1 instanceof Timestamp)
+	        nano1 = ((Timestamp)t1).getNanos ();
+	    int nano2 = ((int) t2.getTime () % 1000) * 1000000;
+	    if (t2 instanceof Timestamp)
+	        nano2 = ((Timestamp)t2).getNanos ();
+
+	    int diffNanos = nano1 - nano2;
+	    if (diffNanos < 0)
+	    {
+	        // Borrow one second
+	        diffSeconds --;
+	        diffNanos += 1000000000;
+	    }
+
+	    // mix nanos and millis again
+	    Timestamp result = new Timestamp ((diffSeconds * 1000) + (diffNanos / 1000000));
+	    // setNanos() with a value of in the millisecond range doesn't affect the value of the time field
+	    // while milliseconds in the time field will modify nanos! Damn, this API is a *mess*
+	    result.setNanos (diffNanos);
+	    return result;
+	}
+}
+
+class TaskLogEntry{
+	LocalDateTime start, stop;
+	Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+	String name;
+	String command;
+	String data;
+	StringTokenizer stock = new StringTokenizer(command, "\n"); {
+		if (stock.countTokens() > 3) 
+			data = stock.nextToken();
+		else
+			data = "";
+	}
+	
+	
+}
+
 class TimeStamp	
 {
-	public static String main() {
+	public static Timestamp main() {
 		//current date and time retrived using DateFormat and Calendar class
+		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
 		DateFormat df = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
 		Calendar calobj = Calendar.getInstance();
-		return (df.format(calobj.getTime()));
+		return (timestamp);
 	}
 }
 class WriteFile{
