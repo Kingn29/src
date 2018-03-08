444b28dbcd49476c037db66444bb151b9d70ddb2
king29 <nking5294@gmail.com>
2018-03-07 19:58:21 -0800

    Added size command capabilities, handled in task class and TaskLogEntry classes

--- docs/TM.java
+++ docs/TM.java
@@ -35,6 +35,9 @@ public class TM
 			String[] taskDescription = Arrays.copyOfRange(args, 2, args.length);
 			log.writeLine(LocalDateTime.now() + "\t" + args[1] + "\t" + "describe" + "\t" + args[2]);
 			break;
+		case "size" :
+			log.writeLine(LocalDateTime.now() + "\t" + args[1] + "\t" + "size" + "\t" + args[2]);
+			break;
 		case "summary" :
 			entries = log.read(); 
 			if (args.length > 1) {
@@ -73,167 +76,7 @@ public class TM
 	}
 	
 	
-/*	private static void cmdStart(WriteFile file, String[] args) throws IOException {
-		/*String ts = LocalDateTime.now().toString();
-		LocalDateTime start = LocalDateTime.parse(ts);
-		saveData(ts, file, args);
-		*/
-		
-		//TaskLogEntry entry = new TaskLogEntry();
-		/*
-	}
-	private static void cmdStop(WriteFile file, String[] args) throws IOException {
-		String ts = LocalDateTime.now().toString();
-		LocalDateTime start = LocalDateTime.parse(ts);
-		saveData(ts, file, args);
-	}
-	private static void cmdDescribe(WriteFile file, String[] args) throws IOException {	
-		String[] description = new String[args.length-2];
-		if (args[0].equalsIgnoreCase("size")) {
-			for (int i = 2; i < args.length; i++) {
-				description[i-2] = args[i];
-			}
-			saveData(args[1] + " Size:", file, description);
-		}
-		else {
-			for (int i = 2; i < args.length; i++) {
-				description[i-2] = args[i];
-			}
-			saveData(args[1] + " Description:", file, description);
-			
-		}
-	}
-	*/
-	//Method retrieved 2/1/2018 from https://stackoverflow.com/questions/16169418/write-a-program-find-that-searches-all-files-specified-on-the-command-line-and-p
-	/*public static String cmdSummary(String[] args) throws FileNotFoundException {	
-		File one = new File("tm.txt");
-		String een = "";
-	    Scanner in = new Scanner(one);
-	    if (args.length > 1) {
-		    String input = args[1];
-		    while (in.hasNextLine()) {
-		        een = in.nextLine();
-		        if (een.contains(input)) {
-		           System.out.println(een);
-		        }
-		    }
-		    return een;
-	    }
-	    else if (args.length == 1) 
-	    {
-	    	while (in.hasNextLine()) {
-		        een = in.nextLine();
-		        System.out.println(een);
-		    }
-	    	return een;
-	    }
-	    in.close();
-		return een;
-	}
-	private static void saveData(String time, WriteFile file, String[] args) throws IOException {
-		file.openFile();
-		file.addText(time, args);
-		file.closeFile();
-		System.out.println("Data has been saved to tm.txt");
-	}
-	*/
 
-	private static void cmdRename(WriteFile file, String args[]) {
-		Scanner sc = new Scanner(System.in);
-		String oldText = "";
-		if (args[0].equalsIgnoreCase("rename")) {
-			oldText = args[1];
-			String newText = args[2];;
-			rename("tm.txt", oldText, newText);
-			System.out.println("Task " + oldText + " now appears as " + newText + " in tm.txt");
-		}
-		else if (args[0].equalsIgnoreCase("delete")){
-			oldText = args[1];
-			String newText = "";
-			rename("tm.txt", oldText, newText);
-		}
-		
-		
-	}
-	//code adapted from http://javaconceptoftheday.com/modify-replace-specific-string-in-text-file-in-java/
-	//on 2/22/18
-	static void rename(String filePath, String oldString, String newString)
-    {
-        File fileToBeModified = new File("StringBuilder summery(LinkedList<TaskLogEntry> entries);tm.txt");
-        String oldContent = "";
-        BufferedReader reader = null;
-        FileWriter writer = null;
-        if (!newString.equals("")) {
-        	 try
-             {
-                 reader = new BufferedReader(new FileReader(fileToBeModified));
-                 //Reading all the lines of input text file into oldContent
-                 String line = reader.readLine();
-                 	while (line != null){
-                 		oldContent = oldContent + line + System.lineSeparator();
-                         line = reader.readLine();
-                         }
-                 //Replacing oldString with newString in the oldContent          
-                 String newContent = oldContent.replaceAll(oldString, newString);             
-                 //Rewriting the input text file with newContent             
-                 writer = new FileWriter(fileToBeModified);             
-                 writer.write(newContent);
-             }
-             catch (IOException e)
-             {
-                 e.printStackTrace();
-             }
-             finally
-             {
-                 try
-                 {
-                     //Closing the resources                 
-                     reader.close();                 
-                     writer.close();
-                 } 
-                 catch (IOException e) 
-                 {
-                     e.printStackTrace();
-                 }
-             }
-        }
-        else if (newString.equals("")) {
-        	 try
-             {
-                 reader = new BufferedReader(new FileReader(fileToBeModified));
-                 //Reading all the lines of input text file into oldContent
-                 String line = reader.readLine();
-                 	while (line != null){
-                 		oldContent = oldContent + line + System.lineSeparator();
-                         line = reader.readLine();
-                         }
-                 //Replacing oldString with newString in the oldContent       
-                 String newContent = oldContent.replaceAll(oldString, "DELETED TASK");             
-                 //Rewriting the input text file with newContent             
-                 writer = new FileWriter(fileToBeModified);             
-                 writer.write(newContent);
-             }
-             catch (IOException e)
-             {
-                 e.printStackTrace();
-             }
-             finally
-             {
-                 try
-                 {
-                     //Closing the resources                 
-                     reader.close();                 
-                     writer.close();
-                 } 
-                 catch (IOException e) 
-                 {
-                     e.printStackTrace();
-                 }
-             }
-        	 System.out.println("Task has been flagged as \"DELETED TASK\" in tm.txt");
-        }
-       
-    }
 }
 
 
@@ -290,15 +133,25 @@ class TaskLog{
 class TaskLogEntry{
 	LocalDateTime timeStamp;
 	String name;
-	String command; 
+	String command;
+	String size;
 	String data;
 	TaskLogEntry(String line) {
 	StringTokenizer stock = new StringTokenizer(line, "\t");
 	timeStamp = LocalDateTime.parse(stock.nextToken());
 	name = stock.nextToken();
 	command = stock.nextToken();
-	while (stock.hasMoreTokens()) 
-		data = stock.nextToken();
+	while (stock.hasMoreTokens()) { 
+		if (command.equals(size)) {
+			size = stock.nextToken();
+		}
+		else {
+			data = stock.nextToken();
+			if (stock.nextToken() != null)
+				size = stock.nextToken();
+		}
+		
+	}
 	}
 }
 
@@ -321,6 +174,7 @@ class TaskDuration{
 class Task{
 	String name;
 	String description;
+	String size = "";
 	long elapsedSeconds;
 	LinkedList<TaskDuration> durations;
 	Task(String name, LinkedList<TaskLogEntry> entries) {
@@ -332,15 +186,18 @@ class Task{
 					switch (entry.command){
 					case "start" : 
 						lastStart = entry.timeStamp;
-					break;
+						break;
 					case "stop" :
 						if (lastStart != null) 
 							addDuration(lastStart, entry.timeStamp);
-							lastStart = null;
-					break;
+						lastStart = null;
+						break;
 					case "describe" :
 						description = entry.data;
-					break;					
+						break;		
+					case "size" :
+						size = entry.size;
+						break;
 					}	
 			}
 		}	
@@ -354,6 +211,7 @@ class Task{
 		String s = "";
 		s = s + "Summary for task	: " + name + "\n";
 		s = s + "Description 		: " + this.description + "\n";
+		s = s + "Size of task 		: " + size + "\n";
 		s = s + "Total Time on Task \t: " + TimeUtil.elapsedTime(elapsedSeconds) + "\n";
 		return s;
 	}
@@ -371,47 +229,5 @@ class TimeUtil{
 		long seconds = TimeUnit.SECONDS.toSeconds(totalSeconds) - (TimeUnit.SECONDS.toMinutes(totalSeconds) *60);
 		String s = (Long.toString(hours) + ":" + Long.toString(minutes) + ":" + Long.toString(seconds));
 		return s;
-	}
-}
-
-class TimeStamp	
-{
-	public static LocalDateTime main() {
-		//current date and time retrived using DateFormat and Calendar class
-		Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-		DateFormat df = new SimpleDateFormat("MM/dd/yy HH:mm:ss");
-		Calendar calobj = Calendar.getInstance();
-		
-		String ts = LocalDateTime.now().toString();
-		LocalDateTime time = LocalDateTime.parse(ts);
-		return (time);
-	}
-	
-}
-class WriteFile{
-	private Formatter form = new Formatter();
-	ArrayList<String> list = new ArrayList<String>();
-	public void openFile(){
-		try {
-			PrintWriter outFile = new PrintWriter(new FileWriter("tm.txt", true));
-			form = new Formatter();
-		}
-		catch(Exception e) {
-			System.out.println("Error with creating file.");
-		}
-	}
-	public void addText(String time, String[] args) throws IOException {
-		list.add(time);
-		for (int i = 0; i < args.length; i++){
-			list.add(args[i]);
-		}
-		List<String> list = new ArrayList<>(Arrays.asList(args));
-		String formattedString = list.toString().replace(",", "").replace("[", "").replace("]", "").trim();          
-		PrintWriter outFile = new PrintWriter(new FileWriter("tm.txt", true));
-		outFile.println(form.format("%s %s", time, formattedString));
-		outFile.close();
-	}
-	public void closeFile() {
-		form.close();
 	}
 }
